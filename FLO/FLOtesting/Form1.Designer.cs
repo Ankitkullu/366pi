17b2c434437dc70bd620a2b507cd77e9a4cda0b4
Ankitkullu <60440337+Ankitkullu@users.noreply.github.com>
2023-02-07 13:45:03 +0530

    flo

---
+++ FLO/FLOtesting/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace FLOtesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.receiver = new System.Windows.Forms.TextBox();
+            this.amount = new System.Windows.Forms.TextBox();
+            this.message = new System.Windows.Forms.TextBox();
+            this.senderTxT = new System.Windows.Forms.TextBox();
+            this.send = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.senderAddress = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblmessage = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // receiver
+            // 
+            this.receiver.Location = new System.Drawing.Point(255, 225);
+            this.receiver.Name = "receiver";
+            this.receiver.Size = new System.Drawing.Size(267, 20);
+            this.receiver.TabIndex = 0;
+            // 
+            // amount
+            // 
+            this.amount.Location = new System.Drawing.Point(255, 317);
+            this.amount.Name = "amount";
+            this.amount.Size = new System.Drawing.Size(267, 20);
+            this.amount.TabIndex = 1;
+            // 
+            // message
+            // 
+            this.message.Location = new System.Drawing.Point(255, 366);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(267, 20);
+            this.message.TabIndex = 2;
+            // 
+            // senderTxT
+            // 
+            this.senderTxT.Location = new System.Drawing.Point(255, 133);
+            this.senderTxT.Name = "senderTxT";
+            this.senderTxT.Size = new System.Drawing.Size(267, 20);
+            this.senderTxT.TabIndex = 3;
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(344, 415);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(75, 23);
+            this.send.TabIndex = 4;
+            this.send.Text = "SEND";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Sender";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Receiver";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Amount";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(167, 373);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Message";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::FLOtesting.Properties.Resources.logo_with_name_v2;
+            this.pictureBox1.Location = new System.Drawing.Point(303, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 84);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // senderAddress
+            // 
+            this.senderAddress.Location = new System.Drawing.Point(255, 174);
+            this.senderAddress.Name = "senderAddress";
+            this.senderAddress.Size = new System.Drawing.Size(267, 20);
+            this.senderAddress.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(126, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Recevier Address";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(255, 274);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(135, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Sender Address";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // lblmessage
+            // 
+            this.lblmessage.AutoSize = true;
+            this.lblmessage.Location = new System.Drawing.Point(626, 155);
+            this.lblmessage.Name = "lblmessage";
+            this.lblmessage.Size = new System.Drawing.Size(0, 13);
+            this.lblmessage.TabIndex = 14;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(614, 225);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(160, 13);
+            this.linkLabel1.TabIndex = 15;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "View transcation on BLockchain";
+            this.linkLabel1.Visible = false;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(614, 168);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(0, 13);
+            this.lbl.TabIndex = 16;
+            this.lbl.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FLOtesting.Properties.Resources.GiantLameHoki_mobile;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lblmessage);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.senderAddress);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.senderTxT);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.amount);
+            this.Controls.Add(this.receiver);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox receiver;
+        private System.Windows.Forms.TextBox amount;
+        private System.Windows.Forms.TextBox message;
+        private System.Windows.Forms.TextBox senderTxT;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox senderAddress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblmessage;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label lbl;
+    }
+}
+
---
+++ FLO/FLOtesting/Form1.cs
@@ -0,0 +1,100 @@
+﻿using FloSDK.Exceptions;
+using FloSDK.Methods;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FLOtesting
+{
+    public partial class Form1 : Form
+    {
+        public static string receiverAddress = "ofewGM44vtMYfszdPQ99tQKDynmvjoAWJW";
+        public string url = "https://testnet.flocha.in/tx/";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            string username = ConfigurationManager.AppSettings.Get("username");
+            string password = ConfigurationManager.AppSettings.Get("password");
+            string wallet_url = ConfigurationManager.AppSettings.Get("wallet_url");
+            string wallet_port = ConfigurationManager.AppSettings.Get("wallet_port");
+
+            RpcMethods obj = new RpcMethods(username, password, wallet_url, wallet_port);
+            try
+            {
+                string Sender = senderTxT.Text;
+                string SenderAddress = senderAddress.Text;
+                string Receiver = receiver.Text;
+                string ReceiverAddress = receiver.Text;
+                string Amount = amount.Text;
+                string Message = message.Text;
+                string flodata = "recevied the amount from " + Sender + " of " + SenderAddress + " for an amount of " + Amount + "FLo to" + Receiver + " of " + ReceiverAddress + " # " + Message;
+
+                JObject jobj = JObject.Parse(obj.SendToAddress(receiverAddress,0.1M,"366pi","366pi",false,false,1,"UNSET",flodata));
+                if (string.IsNullOrEmpty(jobj["error"].ToString()))
+                    {
+                    url += jobj["result"];
+                    lblmessage.Text = "Transaction is successfull";
+                    lblmessage.ForeColor = Color.Blue;
+                    lblmessage.Visible =true;
+                    linkLabel1.Visible =true;
+                    }
+                else
+                {
+                    lblmessage.Text = "error in transaction ";
+                    lblmessage.ForeColor = Color.Red;
+                    lblmessage.Visible = true;
+                    linkLabel1.Visible = false;
+                }
+            }
+            catch (RpcInternalServerErrorException ex)
+            {
+                var errcode = 0;
+                var errMessage = string.Empty;
+
+                if (ex.RpcErrorCode.GetHashCode() != 0) 
+                {
+                    errcode = ex.RpcErrorCode.GetHashCode();
+                    errMessage = ex.RpcErrorCode.ToString();
+                }
+
+                Console.WriteLine("Exception : " + errcode + " - " + errMessage);
+                lblmessage.Text = "error in transaction ";
+                lblmessage.ForeColor = Color.Red;
+                lblmessage.Visible = true;
+                linkLabel1.Visible = false;
+            }
+            catch(Exception ex1) 
+            {
+                Console.WriteLine("Exception : " + ex1.ToString());
+                lblmessage.Text = "error in transaction ";
+                lblmessage.ForeColor = Color.Red;
+                lblmessage.Visible = true;
+                linkLabel1.Visible = false; 
+            }
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(url); 
+        }
+    }
+}
+    
